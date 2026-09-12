c7b6e345c7a9a44239a818b1a1f024a4991f02f6
Fog Night <222979399+FogNight5@users.noreply.github.com>
2026-09-12 21:39:20 +0200

    Add daily restriction and Improve mobile UI

--- Models/Topic.cs
+++ Models/Topic.cs
@@ -4,5 +4,6 @@ public class Topic
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public bool LimitToOncePerDay { get; set; }
     public List<Entry> Entries { get; set; } = new();
 }
